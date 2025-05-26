dea3173e8c8aa6a7bc082bb801f4f4e113d8138f
qiuyixiang <qiuyixiang2003@gmail.com>
2025-05-26 16:04:21 -0500

    add template for testing

--- test/test_python/init/test_file_create.py
+++ test/test_python/init/test_file_create.py
@@ -1,12 +1,23 @@
-import os
-import sys
+"""
+Test case name: test_file_create
+Test case description:
+    Test the file creation after execute the command "gitlet init"
+"""
 
+import os
 import pytest
 
+PROGRAM_NAME = "gitlet"
 
-def test_init_command() -> None:
-    assert 1 == 1
+
+def test_file_create() -> None:
+    """
+    Test the file creation after execute the command "gitlet init"
+    """
+    result = os.system(f"{PROGRAM_NAME} --help")
+    if (result != 0):
+        raise RuntimeError("Missing gitlet program, try to run 'make export-env' to export the program to the PATH variable")
+
 
 if __name__ == "__main__":
     pytest.main()
-
