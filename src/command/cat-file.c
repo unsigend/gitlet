589f9cba07e03916805d91b448ce76f76233c8de
qiuyixiang <qiuyixiang2003@gmail.com>
2025-06-06 11:20:57 -0500

    implement basic frame

--- src/command/cat-file.c
+++ src/command/cat-file.c
@@ -25,25 +25,103 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <stdio.h>
+
+#include <argparse.h>
 
 #include <command/cat-file.h>
 #include <object/object.h>
 #include <object/repository.h>
 #include <util/error.h>
+#include <util/str.h>
 
 void command_cat_file(int argc, char *argv[]) {
-    struct repository repo;
-    char buffer[PATH_MAX];
-    memset(buffer, 0, PATH_MAX);
+    char current_dir[PATH_MAX];
+    memset(current_dir, 0, PATH_MAX);
 
-    if (getcwd(buffer, PATH_MAX) == NULL){
+    if (getcwd(current_dir, PATH_MAX) == NULL){
         gitlet_panic("Failed to get the current working directory");
     }
 
-    repository_object_init(&repo, buffer, true);
-    if (argc < 1){
-        gitlet_panic("No object name provided");
+    struct argparse_description description;
+    description._program_name =  NULL;
+    description._usage = "gitlet cat-file [object]";
+    description._description = "Display the contents of an object";
+    description._epilog = NULL;
+
+    bool t_flag = false;
+    bool p_flag = false;
+    bool s_flag = false;
+    bool e_flag = false;
+
+    struct argparse_option options[] = {
+        OPTION_GROUP("Options"),
+        OPTION_BOOLEAN('t', NULL, "show object type (one of 'blob', 'tree', 'commit', 'tag', ...)", &t_flag, NULL, 0),
+        OPTION_BOOLEAN('p', NULL, "pretty-print the contents of the object", &p_flag, NULL, 0),
+        OPTION_BOOLEAN('s', NULL, "show object size", &s_flag, NULL, 0),
+        OPTION_BOOLEAN('e', NULL, "check if <object> exists", &e_flag, NULL, 0),
+        OPTION_HELP(),
+        OPTION_GROUP_END(),
+        OPTION_END()
+    };
+
+    struct argparse argparse;
+    argparse_init(&argparse, options, &description);
+
+    if (argc == 0){
+        argparse_parse(&argparse, 1, (char *[]){"-h"});
+    }else{
+        argparse_parse(&argparse, argc, argv);
+        if (p_flag && (t_flag || s_flag || e_flag)){
+            gitlet_panic("options  \'-p\' cannot be used with \'-t\', \'-s\', or \'-e\'");
+        }
+        if (s_flag && (t_flag || p_flag || e_flag)){
+            gitlet_panic("options  \'-s\' cannot be used with \'-t\', \'-p\', or \'-e\'");
+        }
+        if (e_flag && (t_flag || p_flag || s_flag)){
+            gitlet_panic("options  \'-e\' cannot be used with \'-t\', \'-p\', or \'-s\'");
+        }
+        if (t_flag && (p_flag || s_flag || e_flag)){
+            gitlet_panic("options  \'-t\' cannot be used with \'-p\', \'-s\', or \'-e\'");
+        }
+
+        struct repository repo;
+        repository_object_init(&repo, current_dir, true);
+
+        struct object obj;
+        const char * sha1 = argv[argc - 1];
+        
+        if (!isxdigit(sha1[0]) || strlen(sha1) != 40){
+            gitlet_panic("Not a valid object name: %s", sha1);
+        }
+        object_init(&obj, &repo, sha1);
+
+        if (t_flag){
+            switch (obj.type){
+                case OBJECT_TYPE_BLOB:
+                    printf("blob\n");
+                    break;
+                case OBJECT_TYPE_TREE:
+                    printf("tree\n");
+                    break;
+                case OBJECT_TYPE_COMMIT:
+                    printf("commit\n");
+                    break;
+                case OBJECT_TYPE_TAG:
+                    printf("tag\n");
+                    break;
+                default:
+                    gitlet_panic("Unknown object type: %s", obj.type);
+            }
+        }
+        else if (p_flag){
+            printf("%s\n", obj.content);
+        }
+        else if (s_flag){
+            printf("%llu\n", obj.file_size);
+        }
     }
-    struct object _object;
-    object_init(&_object, &repo, argv[0]);
+    
 }
